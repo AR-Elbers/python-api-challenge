12847066005c585d4d51d0f7777a9a2620ee4262
AR-Elbers <70582059+AR-Elbers@users.noreply.github.com>
2021-01-26 20:17:03 +0800

    Finalised VacationPy and removed API Keys

--- VacationPy/Config.py
+++ VacationPy/Config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-api_key_owm= "b80fd2ed7964ad60c921362b393b0613"
+api_key_owm= "enter key here"
 
 # Google API Key
-api_key_g = "YOUR KEY HERE!"
+api_key_g = "enter key here"
